225eb65b666f15bd772b73a8768e7c9dff6d7cea
Eszter Juharos <juharoseszter@gmail.com>
2023-12-19 10:46:35 +0100

    Add comments to feladat3

--- feladat3/paralel.ts
+++ feladat3/paralel.ts
@@ -1,3 +1,5 @@
+// A feladat elkészítéséhez két függvényre van szükség, amik a bemenő adatokkal kiszámítják a 
+// megfelelő módon a kért területeket.
 function paralelogrammaTeruletMagassaggal(oldal: number, magassag: number): number {
     return oldal * magassag;
 }
@@ -7,7 +9,8 @@ function paralelogrammaTeruletKetOldallalEsSzoggel(oldalA: number, oldalB: numbe
     return oldalA * oldalB * szinuszSzog;
 }
 
-// Példa használat:
+// A használathoz értéket kell adni a függvényben szereplő változóknak, majd meg kell hívni a
+// függvényeket. Hogy szép legyen az output, a konzolba szöveget is kiiratok az eredményeken kívül.
 const oldal = 5;
 const magassag = 8;
 const terulet1 = paralelogrammaTeruletMagassaggal(oldal, magassag);
@@ -15,8 +18,9 @@ console.log(`A paralelogramma területe a megadott magassággal: ${terulet1}`);
 
 const oldalA = 7;
 const oldalB = 9;
-const szog = 45; // Például 45 fokos szög
+const szog = 45;
 const terulet2 = paralelogrammaTeruletKetOldallalEsSzoggel(oldalA, oldalB, szog);
 console.log(`A paralelogramma területe a megadott két oldallal és szöggel: ${terulet2}`);
 
+// Ez a lépés azért szükséges, hogy a js file megfelelően generálódjon le.
 export {};
